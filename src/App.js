76b4f5f9437381e6172ea024038b77c5224e1abc
Szydlowskyyy <szydlowsky.studio@gmail.com>
2023-07-10 13:28:37 +0200

    Currency v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [amount, setAmount] = useState(0);
+  const [result, setResult] = useState("");
+  const [currencyFrom, setCurrencyFrom] = useState("USD");
+  const [currencyTo, setCurrencyTo] = useState("PLN");
+
+  useEffect(
+    function () {
+      if (!amount) return;
+      const controller = new AbortController();
+
+      async function fetchCurrency() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${currencyFrom}&to=${currencyTo}`,
+          { signal: controller.signal }
+        );
+
+        const data = await res.json();
+        setResult(data.rates[Object.keys(data.rates)[0]]);
+      }
+
+      fetchCurrency();
+
+      return () => controller.abort();
+    },
+    [amount, currencyFrom, currencyTo]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="container App">
+        <div className="currency-control">
+          <input
+            type="text"
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+
+          <div className="currency-select">
+            <Selection
+              currencyValue={currencyFrom}
+              onSetCurrency={setCurrencyFrom}
+            />
+
+            <p className="arrow">&rarr;</p>
+
+            <Selection
+              currencyValue={currencyTo}
+              onSetCurrency={setCurrencyTo}
+            />
+          </div>
+        </div>
+
+        <Result amount={amount} result={result} />
+      </div>
+
+      {amount && result ? (
+        <Details
+          amount={amount}
+          currencyFrom={currencyFrom}
+          currencyTo={currencyTo}
+          result={result}
+        />
+      ) : null}
+    </>
+  );
+}
+
+function Result({ amount, result }) {
+  return (
+    <div className="result">
+      <p>{amount && result ? result.toFixed(2) : "Start!"}</p>
     </div>
   );
 }
 
-export default App;
+function Selection({ currencyValue, onSetCurrency }) {
+  return (
+    <select
+      value={currencyValue}
+      onChange={(e) => onSetCurrency(e.target.value)}
+    >
+      <option value={"USD"}>USD</option>
+      <option value={"EUR"}>EUR</option>
+      <option value={"CAD"}>CAD</option>
+      <option value={"INR"}>INR</option>
+      <option value={"PLN"}>PLN</option>
+    </select>
+  );
+}
+
+function Details({ amount, currencyFrom, currencyTo, result }) {
+  const currencyCalculated = (result / amount).toFixed(2);
+  return (
+    <p className="details">
+      1 {currencyFrom} is {currencyCalculated} {currencyTo}
+    </p>
+  );
+}
